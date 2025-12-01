1ec3924bedbaf550cc6d4a18fb392a734061a23c
MuxuZhao <zhaomx1129@163.com>
2025-11-30 21:56:02 -0600

    added new album

--- src/data/stories-loader.js
+++ src/data/stories-loader.js
@@ -1,12 +1,12 @@
 // src/data/stories-loader.js
 
 import onCampus from './stories/onCampus.json';
-//import story2 from './stories/story2.json';
+import roadTrip from './stories/roadTrip.json';
 // import story3 from './stories/story3.json';
 
 // export
 export const storiesData = [
   onCampus,
-  // story2,
+  roadTrip
   // story3
 ];
